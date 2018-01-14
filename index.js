6df71b5a25269fcb1f644a8716643e31230ed1e1
Grace Shih <vocalise@gmail.com>
2018-01-14 02:56:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function destructivelyAppendKitten(kitten) {
   kittens.push(kitten);
   return kittens;
 }
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  return kittens;
+}
