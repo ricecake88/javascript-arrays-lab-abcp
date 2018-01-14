1e93f99b1c87fd3c1a07704c631f9bd54ed65f45
Grace Shih <vocalise@gmail.com>
2018-01-14 03:02:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,3 +35,8 @@ function removeLastKitten() {
   var newKittens = kittens.slice(0, kittens.length-1);
   return newKittens;
 }
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1, kittens.length);
+  return newKittens;
+}
