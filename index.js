0fb35a98d4b51d51b5bc82c5fd8e578508e470d0
Grace Shih <vocalise@gmail.com>
2018-01-14 03:02:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+  return kittens;
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(kitten) {
+  var newKittens = [...kittens, kitten];
+  return newKittens;
+}
+
+function prependKitten(kitten) {
+  var newKittens = [kitten, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length-1);
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1, kittens.length);
+  return newKittens;
+}
